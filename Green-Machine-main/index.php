6515449b7e7196e8b07cf558673b29ba4ede72e2
Courage Genito <courage_genito@dlsu.edu.ph>
2025-04-07 16:25:30 +0800

    Changed the picture

--- Green-Machine-main/index.php
+++ Green-Machine-main/index.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Drinks</title>
+    <title>Green Machine</title>
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
     <header class="fixed-header">
         <div class ="headerContainer">
-            <img src="images/logo.png" class="headerPic">
+            <img src="assets/images/logo.png" class="headerPic">
         </div>
         <form action="search.php" method="get" class="search-form">
             <input type="text" name="query" placeholder="Search items..." required>
-            <button type="submit"><img src="images/search.png" class="searchPic"></button>
+            <button type="submit"><img src="assets/images/search.png" class="searchPic"></button>
         </form>
         <img src="assets/images/cart.png" class="cartPic">
         <nav>
@@ -23,5 +23,8 @@
         </nav>
     </header>
     <script src="script.js"></script>
+    <div class="container">
+        
+    </div>
 </body>
 </html>
