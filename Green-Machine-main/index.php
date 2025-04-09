5121076143f20dda79200bcf691246983215569d
Courage Genito <courage_genito@dlsu.edu.ph>
2025-04-09 22:29:42 +0800

    asdfdcdd

--- Green-Machine-main/index.php
+++ Green-Machine-main/index.php
@@ -45,15 +45,17 @@ session_start();
     ?>
 
 
-    <div class="items-container">
-        <?php while ($row = $items->fetch_assoc()): ?>
-            <div class="item" data-name="<?= $row['item'] ?>" data-price="<?= $row['price'] ?>">
-                <strong><?= $row['item'] ?></strong><br>
-                ₱<?= number_format($row['price'], 2) ?>
-            </div>
-        <?php endwhile; ?>
-    </div>
-
+<div class="items-container">
+    <?php while ($row = $items->fetch_assoc()): ?>
+        <div class="item" data-name="<?= htmlspecialchars($row['item']) ?>" data-price="<?= $row['price'] ?>">
+            <img src="<?= !empty($row['image_path']) ? htmlspecialchars($row['image_path']) : 'assets/images/default-product.png' ?>" 
+                 alt="<?= htmlspecialchars($row['item']) ?>"
+                 onerror="this.onerror=null;this.src='assets/images/default-product.png'">
+            <strong><?= htmlspecialchars($row['item']) ?></strong><br>
+            ₱<?= number_format($row['price'], 2) ?>
+        </div>
+    <?php endwhile; ?>
+</div>
     <div id="popup-bg"></div>
     <div id="popup">
         <div style="display: flex;">
